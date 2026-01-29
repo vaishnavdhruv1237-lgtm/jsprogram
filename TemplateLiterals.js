831cc234f85507592633901734d67d22cdae1cd7
Dhruv <vaishnavdhruv1237@gmail.com>
2026-01-29 09:08:55 +0530

    [PU][added two files]

---
+++ TemplateLiterals.js
@@ -0,0 +1,4 @@
+let fName = "Dhruv";
+let lName = "Vaishnav";
+let message = `Hello Sudents, i am ${fName} ${lName}`
+console.log(message);
---
+++ methods.js
@@ -0,0 +1,37 @@
+// String methods
+
+// length()
+// trim()
+// toUpperCase()
+// toLowerCase()
+// includes()
+// indexOf()
+// slice()
+// substring()
+// replace()
+// concat()
+// split()
+
+let str = " Hello JavaScript World ! "
+console.log(str)
+console.log("length() : ",str.length)
+
+let trimText = str.trim();
+console.log("trim() : ", trimText.length)
+
+console.log("toUpperCase() : ", str.toUpperCase())
+console.log("toLowerCase() : ", str.toLowerCase())
+
+console.log("length() : ", str.includes("JavaScript"))
+
+console.log("indexOf() : ", str.indexOf("JavaScript"))
+
+console.log("slice() : ", str.slice(2,7))
+
+console.log("subString() : ", str.substring(2,7))
+
+console.log("replace() : ", str.replace("JavaScript","React"))
+
+console.log("concat() : ", str.concat("Hello ReactJS"))
+
+console.log("split() : ", str.split("Hello"))
