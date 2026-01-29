ea33af40469b87a0ec9b75a94145a98848a64d57
Dhruv <vaishnavdhruv1237@gmail.com>
2026-01-29 12:08:39 +0530

    [PU][added two files]

--- Obj.js
+++ Obj.js
@@ -1,1 +1,41 @@
 // object is the collection of any type of data
+
+const person = {
+    name : 'testing',
+    age: 24,
+    hobbies: ['cricket','listen music'],
+    greet: function(){
+        console.log("hello")
+    }
+}
+
+// access values from object
+
+console.log(person.name)
+console.log(person['name'])
+
+// adding or modifying the data of obj
+
+person.job = "react developer"
+person.salary = 50000
+console.log(person)
+
+delete person.salary
+console.log(person)
+
+// nested object
+
+const person1 = {
+    name: 'john',
+    age: 30,
+    address: {
+        city: "junagadh",
+        zipcode: '362001',
+        street: "123 main st."
+    }
+}
+console.log(person1)
+console.log(person1?.address?.city)
+console.log(Object.keys(person1))
+console.log(Object.values(person1))
+console.log(Object.entries(person1))
